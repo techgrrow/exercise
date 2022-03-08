991f45296c9040b435847be2054fdadf77093d3e
Luca Magnani <lucamagnani@digitalmens.it>
2022-03-08 21:48:35 +0100

    edit args + basic validation

--- todo_manager.py
+++ todo_manager.py
@@ -6,14 +6,15 @@ import calendar
 import time
 
 todo_list = []
-todo_db = "todo.db" #db file path
+todo_present = False
+todo_db = "todo.db"  # db file path
 
-#adding arguments
+# adding arguments
 parser = argparse.ArgumentParser()
 
 parser.add_argument("-ls", "--list", dest="list", action='store_true', help="show all todos sorted by added date desc.")
 parser.add_argument("-a", "--add", dest="add", help="add todo (params: title)")
-parser.add_argument("-e", "--edit", dest="edit", help="edit todo (params: id, title)")
+parser.add_argument("-e", "--edit", nargs='+', dest="edit", help="edit todo (params: id, title)")
 parser.add_argument("-d", "--delete", dest="delete", help="delete todo (params: id, title)")
 parser.add_argument("-t", "--toggle", dest="toggle", help="toggle done state: True/False (params: id, title)")
 parser.add_argument("-s", "--search", dest="search", help="search todo titles for inserted string "
@@ -25,20 +26,22 @@ the -h/---help argument is already present in this library
 args = parser.parse_args()
 
 
+def check_no_todo():
+    global todo_list, todo_db, todo_present
+    try:
+        todo_present = False if os.stat(todo_db).st_size == 0 else True
+    except:
+        open(todo_db, 'w')  # creates file if not present
+
 
 def load_todos():
     global todo_list, todo_db
     try:
-        if os.stat(todo_db).st_size != 0:
-            try:
-                with open(todo_db, 'r') as file:
-                    file_content = file.read()
-                    todo_list = list(eval(file_content))
-            except Exception as e:
-                print('Error loading db file: ', e)
-    except:
-        open(todo_db, 'w') #creates file if not present
-
+        with open(todo_db, 'r') as file:
+            file_content = file.read()
+            todo_list = list(eval(file_content))
+    except Exception as e:
+        print('Error loading db file: ', e)
 
 
 def get_timestamp():
@@ -56,9 +59,22 @@ def write_to_file(content):
         print('Error loading db file: ', e)
 
 
+def validate_id(note_id):
+    if todo_present:
+        try:
+            if note_id.isnumeric() and int(note_id) <= len(todo_list):
+                return int(note_id)
+        except Exception as e:
+            print('Error with todo id inserted: ', e)
+    else:
+        print("No todo inserted yet. Add a new Todo.")
+
+
 if __name__ == '__main__':
 
-    load_todos()
+    check_no_todo()
+    if todo_present:
+        load_todos()
 
     if args.list:
         if os.stat(todo_db).st_size == 0:
@@ -67,7 +83,7 @@ if __name__ == '__main__':
             print(todo_list)
 
     if args.add:
-        todo = {'id': len(todo_list),
+        todo = {'id': len(todo_list) + 1,
                 'title': args.add,
                 'done': False,
                 'timestamp': get_timestamp()}
@@ -75,7 +91,16 @@ if __name__ == '__main__':
         write_to_file(todo_list)
 
     if args.edit:
-        pass
+        todo_id = validate_id(args.edit[0])
+        if todo_present and todo_id:
+            title = args.edit[1]
+            if len(title) > 4:
+                todo_list[todo_id - 1]['title'] = title
+                write_to_file(todo_list)
+            else:
+                print('Title must be 5 or more characters')
+        else:
+            print("No todo to edit or ID not valid.")
 
     if args.delete:
         pass
@@ -85,4 +110,3 @@ if __name__ == '__main__':
 
     if args.search:
         pass
-
