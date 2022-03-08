a00709840e0f8b0b4710db4d1011b168f89f0279
Luca Magnani <lucamagnani@digitalmens.it>
2022-03-08 13:53:13 +0100

    added basic args, main

--- todo_manager.py
+++ todo_manager.py
@@ -1,2 +1,42 @@
 # -*- coding: utf-8 -*-
 
+import argparse
+
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("h", help="show all available actions")
+parser.add_argument("ls", help="show all todos sorted by added date desc.")
+parser.add_argument("a", help="add todo (params: title)")
+parser.add_argument("e",  help="edit todo (params: id, title)")
+parser.add_argument("d", help="delete todo (params: id, title)")
+parser.add_argument("t", help="toggle done state: True/False (params: id, title)")
+parser.add_argument("s", help="search todo titles for inserted string (params: <search string>)")
+
+args = parser.parse_args()
+
+
+
+if __name__ == '__main__':
+
+    if args.h:
+        pass
+
+    if args.ls:
+        pass
+
+    if args.a:
+        pass
+
+    if args.e:
+        pass
+
+    if args.d:
+        pass
+
+    if args.t:
+        pass
+
+    if args.s:
+        pass
+
